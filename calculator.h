528d9652b0b43418ce184f3720580343333d03ae
3iky <s.little7733@edmail.edcc.edu>
2015-11-16 16:20:00 -0800

    Update calculator.h

--- calculator.h
+++ calculator.h
@@ -3,13 +3,10 @@
 #define QUIT		5
 #define MAX 256
 
-int menu(void);
+double payment_amount_calc(double adjusted_interest_rate, double get_loan_amount, int get_number_of_months);
 
-double payment_amount_calc(double get_interest_rate, double get_loan_amount, int get_number_of_months);
+double loan_amount_calc(double get_payment_amount, double get_interest_rate, int get_number_of_months);
 
-int number_of_months_calc(double get_payment_amount, double get_interest_rate, double get_loan_amount, int get_number_of_months);
+int number_of_months_calc(double get_payment_amount, double get_interest_rate, double get_loan_amount);
 
-double loan_amount_calc(double get_loan_amount, double get_interest_rate, double get_payment_amount, int get_number_of_months);
-
-int number_of_months(int get_number_of_months);
 #endif
